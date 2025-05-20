10ba90216b87a708edc77ccac2bbab643b1f04df
Daniel Shahovtsov <daniil.shahovtsov@yandex.ru>
2025-05-20 14:57:11 +0300

    Исправление первой ошибки

--- backend/src/order/order.service.ts
+++ backend/src/order/order.service.ts
@@ -8,10 +8,19 @@ export class OrderService {
 
   async create(makeOrderDto: MakeOrderDto) {
     const tickets = makeOrderDto.tickets;
+    const dataBase = await this.filmsRepository.findAll();
     const total: number = tickets.length;
     const seats: string[] = tickets.map((res) => {
-      return `${res.row}:${res.seat}`;
+      return `${res.row}:${res.seat}`; 
     });
+ 
+    const takenSeats = dataBase.map(el => el.schedule.flatMap(s => s.taken)).flat();
+    const isSeatTaken = seats.some(seat => takenSeats.includes(seat));
+
+    if (isSeatTaken) {
+      throw new Error('Некоторые билеты на указанные места уже заняты.');
+    }
+
     const result = {
       items: tickets,
       total: total,
