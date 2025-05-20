7a15ca3c323cbd5055b01ae0a7b1dbfe5f01f08f
Daniel Shahovtsov <daniil.shahovtsov@yandex.ru>
2025-05-20 15:38:45 +0300

    Полное исправление

--- backend/src/order/order.service.ts
+++ backend/src/order/order.service.ts
@@ -13,12 +13,21 @@ export class OrderService {
     const seats: string[] = tickets.map((res) => {
       return `${res.row}:${res.seat}`; 
     });
- 
-    const takenSeats = dataBase.map(el => el.schedule.flatMap(s => s.taken)).flat();
-    const isSeatTaken = seats.some(seat => takenSeats.includes(seat));
+
+    const filmBase = dataBase.find(el => {
+      return el.id === tickets[0].film;
+    });
+
+    const scheduleBase = filmBase.schedule.find(el => {
+      console.log(el.id + ' ' +  tickets[0].session);
+      return el.id === tickets[0].session;
+    }).taken;
+
+    console.log(scheduleBase);
+    const isSeatTaken = seats.some(seat => scheduleBase.includes(seat));
 
     if (isSeatTaken) {
-      throw new Error('Некоторые билеты на указанные места уже заняты.');
+      throw new Error('Некоторые билеты на указанные места уже заняты.'); 
     }
 
     const result = {
