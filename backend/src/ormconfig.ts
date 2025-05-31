d36b6de56eaa14529d402d52e10f835dd5c6f7c9
Daniel Shahovtsov <daniil.shahovtsov@yandex.ru>
2025-05-31 16:58:03 +0300

    Еще одна оч попытка

--- backend/src/ormconfig.ts
+++ backend/src/ormconfig.ts
@@ -2,11 +2,11 @@ import { DataSource } from 'typeorm';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
+  host: process.env.DATABASE_HOST,
   port: 5432,
   username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  database: 'nest_project',
+  password: process.env.DATABASE_PASSWORD, 
+  database: process.env.DATABASE,
   entities: [`${__dirname}/**/**/*.entity.{ts,js}`],
   synchronize: true,
 });
