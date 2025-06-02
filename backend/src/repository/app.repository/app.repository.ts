72cb59cc49633da856b3f591f76f6561de4b39f3
Daniel Shahovtsov <daniil.shahovtsov@yandex.ru>
2025-06-02 21:58:53 +0300

    Попытка словить ошибку

--- backend/src/repository/app.repository/app.repository.ts
+++ backend/src/repository/app.repository/app.repository.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Films } from 'src/films/films.entity';
 import { Schedules } from 'src/films/schedules.entity';
@@ -22,7 +22,8 @@ export class AppRepository {
       });
     } catch (error) {
       const errorMessage = 'Ошибка при поиске фильмов: ' + error.message;
-      throw new Error(errorMessage);
+      Logger.error(errorMessage, error.stack); 
+      throw new HttpException(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 
