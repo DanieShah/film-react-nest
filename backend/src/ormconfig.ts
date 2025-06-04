79f14274f6950f31ee22c197f86fb3dd7964c9fc
Daniel Shahovtsov <daniil.shahovtsov@yandex.ru>
2025-06-04 16:01:40 +0300

    Попытка исправить ошибку 200

--- backend/src/ormconfig.ts
+++ backend/src/ormconfig.ts
@@ -3,10 +3,10 @@ import { DataSource } from 'typeorm';
 export const AppDataSource = new DataSource({
   type: 'postgres',
   host: process.env.DATABASE_HOST,
-  port: 5432,
+  port: Number(process.env.DATABASE_PORT),
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE,
   entities: [`${__dirname}/**/**/*.entity.{ts,js}`],
-  synchronize: true,
+  synchronize: false,
 });
