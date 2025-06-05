c8336c75845ca1ff31a2d307679848a7cac38ea0
Daniel Shahovtsov <daniil.shahovtsov@yandex.ru>
2025-06-05 11:42:19 +0300

    Захардкодим базу

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -24,7 +24,7 @@ import { AppRepository } from './repository/app.repository/app.repository';
       port: Number(process.env.DATABASE_PORT),
       username: process.env.DATABASE_USERNAME,
       password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_BASE,
+      database: 'nest_project',
       entities: [Films, Schedules],
       synchronize: false,
     }),
--- backend/src/ormconfig.ts
+++ backend/src/ormconfig.ts
@@ -6,7 +6,7 @@ export const AppDataSource = new DataSource({
   port: Number(process.env.DATABASE_PORT),
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_BASE,
+  database: 'nest_project',
   entities: [`${__dirname}/**/**/*.entity.{ts,js}`],
   synchronize: false,
 });
