789fceb3ab815faaad0824639dcd5a60d2444410
Daniel Shahovtsov <daniil.shahovtsov@yandex.ru>
2025-06-05 16:52:32 +0300

    Создаем пользователя postgres

--- backend/test/prac.init.sql
+++ backend/test/prac.init.sql
@@ -1,7 +1,7 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 create database nest_project
-    with owner student;
+    with owner postgres;
 
 create table public.films
 (
@@ -19,7 +19,7 @@ create table public.films
 );
 
 alter table public.films
-    owner to student;
+    owner to postgres;
 
 create table public.schedules
 (
@@ -38,6 +38,6 @@ create table public.schedules
 );
 
 alter table public.schedules
-    owner to student;
+    owner to postgres;
 
 
